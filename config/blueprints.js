d82ab2e64e6b224930885014f33a7d35809fcb79
Ian Jennings <ian@meetjennings.com>
2020-06-24 20:47:18 -0500

    Paircast - https://app.paircast.io/c/OAoQFWfPZ/967261

--- config/blueprints.js
+++ config/blueprints.js
@@ -36,6 +36,6 @@ module.exports.blueprints = {
   *                                                                          *
   ***************************************************************************/
 
-  shortcuts: false,
+  shortcuts: true,
 
 };
