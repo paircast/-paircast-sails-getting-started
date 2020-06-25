afda53a51778f5a49515889b8c64b01373b6a193
Ian Jennings <ian@meetjennings.com>
2020-06-24 20:51:55 -0500

    Paircast - https://app.paircast.io/c/OAoQFWfPZ/1244346

--- assets/js/pages/hello-world.page.js
+++ assets/js/pages/hello-world.page.js
@@ -10,7 +10,7 @@ parasails.registerPage('hello-world', {
     // whenever question changes, this function will run
     name: async (newName, oldName) => {
       console.log('test')
-      await Cloud.updateProfile({ fullName: newName })
+      await Cloud.updateProfilewith({ fullName: newName })
     }
   },
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
