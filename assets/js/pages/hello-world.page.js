9d980eece71572129e74d5fdc847831b4662dbe8
Ian Jennings <ian@meetjennings.com>
2020-06-24 20:39:22 -0500

    Paircast - https://app.paircast.io/c/OAoQFWfPZ/491536

--- assets/js/pages/hello-world.page.js
+++ assets/js/pages/hello-world.page.js
@@ -9,11 +9,14 @@ parasails.registerPage('hello-world', {
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-  beforeMount: function() {
+  beforeMount: function () {
     // Attach any initial data from the server.
     _.extend(this, SAILS_LOCALS);
   },
-  mounted: async function() {
+  mounted: async function () {
+
+    console.log(this.beta)
+
     //…
   },
 
